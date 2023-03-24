4099cc59f2400159b9275785ece49cfc62ac9cb0
Asguho <78410939+Asguho@users.noreply.github.com>
2023-03-24 12:58:24 +0100

    copied asguhoclient

---
+++ modinstaller.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+use strict;
+my @modrinthMods = ('exordium', 'fabric-api','c2me-fabric','debugify','ebe','fastload','ferritecore-fabric','carpet','ImmediatelyFast','krypton','lazydfu','lithium','starlight','smoothboot-fabric','vmp-fabric', 'modmenu', 'sodium-extra','reeses-sodium-options', 'mouse-wheelie','memoryleakfix','no-chat-reports', 'sodium','mixintrace', 'mixin-conflict-helper', 'notenoughcrashes', 'yosbr');
+#multiconnect
+for(@modrinthMods){
+	system("packwiz mr install $_ -y");
+}
+
+for(@curseforgeMods){
+	system("packwiz cf install $_ -y");
+}
+
+
